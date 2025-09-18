5b63bfaab2288b3501ab6a690b0823c26dc4d3ce
Mesut ILICA <mesut.ilica@gmail.com>
2025-09-18 22:34:01 +0300

    18-09-25 slider

--- SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
+++ SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Data;
+using System.Security.Claims;
 
 namespace SH1ProjeUygulamasi.WebUI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly DatabaseContext _context;
+
+        public AccountController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -11,6 +21,29 @@ namespace SH1ProjeUygulamasi.WebUI.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var kullanici = _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password && u.IsActive);
+            if (kullanici != null)
+            {
+                var haklar = new List<Claim>() // kullanıcı hakları tanımladık
+                    {
+                        new(ClaimTypes.Email, kullanici.Email), // claim = hak(kullanıcıya tanımlalan haklar)
+                        new(ClaimTypes.Role, "Admin")
+                    };
+                var kullaniciKimligi = new ClaimsIdentity(haklar, "Login"); // kullanıcı için bir kimlik oluşturduk
+                ClaimsPrincipal claimsPrincipal = new(kullaniciKimligi);
+                HttpContext.SignInAsync(claimsPrincipal); // yukardaki yetkilerle sisteme giriş yaptık
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(); // çıkış yap
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ SH1ProjeUygulamasi.WebUI/ViewComponents/Kategori.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Data;
+
+namespace SH1ProjeUygulamasi.WebUI.ViewComponents
+{
+    public class Kategori : ViewComponent
+    {
+        private readonly DatabaseContext _context;
+
+        public Kategori(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Categories);
+        }
+    }
+}
