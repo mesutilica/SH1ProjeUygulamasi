1425f578a7563e8a285040021a3fa2103705a9b8
Mesut ILICA <mesut.ilica@gmail.com>
2025-09-24 22:33:03 +0300

    24-09-25 iletiş

--- SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
+++ SH1ProjeUygulamasi.WebUI/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Core.Entities;
 using SH1ProjeUygulamasi.Data;
 using System.Security.Claims;
 
@@ -44,6 +45,34 @@ namespace SH1ProjeUygulamasi.WebUI.Controllers
         {
             HttpContext.SignOutAsync(); // çıkış yap
             return RedirectToAction("Index", "Home");
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    user.IsActive = true;
+                    user.IsAdmin = false;
+                    _context.Users.Add(user);
+                    _context.SaveChanges();
+                    TempData["Message"] = @"<div class=""alert alert-success alert-dismissible fade show"" role=""alert"">
+  <strong>Kayıt işlemi başarılı! Giriş yapabilirsiniz.</strong> 
+  <button type=""button"" class=""btn-close"" data-bs-dismiss=""alert"" aria-label=""Close""></button>
+</div> ";
+                    return RedirectToAction("Login", "Account");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Kayıt sırasında bir hata oluştu!");
+                }                
+            }
+            return View(user);
         }
     }
 }
--- SH1ProjeUygulamasi.WebUI/Controllers/HomeController.cs
+++ SH1ProjeUygulamasi.WebUI/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace SH1ProjeUygulamasi.WebUI.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult ContactUs(string nameSurname, string email, string message)
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
