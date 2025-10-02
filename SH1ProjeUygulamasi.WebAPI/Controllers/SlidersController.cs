c201048adc3ff2010b46dc60997fc9d699806e60
Mesut ILICA <mesut.ilica@gmail.com>
2025-10-02 22:35:46 +0300

    2-10-25 web api entry

---
+++ SH1ProjeUygulamasi.WebAPI/Controllers/SlidersController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SH1ProjeUygulamasi.Core.Entities;
+using SH1ProjeUygulamasi.Service.Abstract;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SH1ProjeUygulamasi.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SlidersController : ControllerBase
+    {
+        private readonly IService<Slider> _service;
+
+        public SlidersController(IService<Slider> service)
+        {
+            _service = service;
+        }
+
+        // GET: api/<SlidersController>
+        [HttpGet]
+        public async Task<IEnumerable<Slider>> Get()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET api/<SlidersController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Slider>> GetAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return Ok(model);
+        }
+
+        // POST api/<SlidersController>
+        [HttpPost]
+        public async Task PostAsync([FromBody] Slider value)
+        {
+            await _service.AddAsync(value);
+            await _service.SaveChangesAsync();
+        }
+
+        // PUT api/<SlidersController>/5
+        [HttpPut("{id}")]
+        public async Task PutAsync(int id, [FromBody] Slider value)
+        {
+            _service.Update(value);
+            await _service.SaveChangesAsync();
+        }
+
+        // DELETE api/<SlidersController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _service.Delete(model);
+            await _service.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ SH1ProjeUygulamasi.WebAPI/Program.cs
@@ -0,0 +1,45 @@
+
+using SH1ProjeUygulamasi.Data;
+using SH1ProjeUygulamasi.Service.Abstract;
+using SH1ProjeUygulamasi.Service.Concrete;
+
+namespace SH1ProjeUygulamasi.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Services.AddDbContext<DatabaseContext>();
+
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddTransient<IProductService, ProductService>();
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped(typeof(IService<>), typeof(Service<>)); // Generic Servis
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
